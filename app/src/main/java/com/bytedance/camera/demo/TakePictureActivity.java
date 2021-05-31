b8ea4a1642dbe0398a27ee4e8af972c8451bb4da
dylanhz <215804619@qq.com>
2021-05-31 22:29:25 +0800

    feat: request multiple permissions

--- app/src/main/java/com/bytedance/camera/demo/TakePictureActivity.java
+++ app/src/main/java/com/bytedance/camera/demo/TakePictureActivity.java
@@ -5,19 +5,20 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class TakePictureActivity extends AppCompatActivity {
 
-    private ImageView imageView;
     private static final int REQUEST_IMAGE_CAPTURE = 1;
-
     private static final int REQUEST_EXTERNAL_STORAGE = 101;
+    private static final int REQUEST_ALL = 255;
+    private ImageView imageView;
 
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_picture);
@@ -28,7 +29,10 @@ public class TakePictureActivity extends AppCompatActivity {
                     Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
                     || ContextCompat.checkSelfPermission(TakePictureActivity.this,
                     Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                //todo 在这里申请相机、存储的权限
+                //todo 在这里申请相机、存储的权限 !done
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        REQUEST_ALL);
             } else {
                 takePicture();
             }
@@ -37,6 +41,7 @@ public class TakePictureActivity extends AppCompatActivity {
     }
 
     private void takePicture() {
+        Toast.makeText(this, "success", Toast.LENGTH_LONG).show();
         //todo 打开相机
     }
 
@@ -52,17 +57,27 @@ public class TakePictureActivity extends AppCompatActivity {
         //todo 展示图片
         //todo 根据imageView裁剪
         //todo 根据缩放比例读取文件，生成Bitmap
-
         //todo 如果存在预览方向改变，进行图片旋转
-
         //todo 如果存在预览方向改变，进行图片旋转
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode) {
-            case REQUEST_EXTERNAL_STORAGE: {
-                //todo 判断权限是否已经授予
+            case REQUEST_ALL: {
+                //todo 判断权限是否已经授予 !done
+                if (grantResults.length == 2) {
+                    if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                        Toast.makeText(this, "拍照权限获取失败", Toast.LENGTH_LONG).show();
+                    }
+                    if (grantResults[1] == PackageManager.PERMISSION_DENIED) {
+                        Toast.makeText(this, "存储权限获取失败", Toast.LENGTH_LONG).show();
+                    }
+                    if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, "权限获取成功", Toast.LENGTH_LONG).show();
+                        takePicture();
+                    }
+                }
                 break;
             }
         }
