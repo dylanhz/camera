ff7971d14f9a4ce6dec69a225c2b1a352ab92874
dylanhz <215804619@qq.com>
2021-06-03 22:59:14 +0800

    feat: take photo & crop feat: save to external storage feat: refresh album feat: rotate image

--- app/src/main/java/com/bytedance/camera/demo/TakePictureActivity.java
+++ app/src/main/java/com/bytedance/camera/demo/TakePictureActivity.java
@@ -3,19 +3,29 @@ package com.bytedance.camera.demo;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.bytedance.camera.demo.utils.Utils;
+
+import java.io.File;
 
 public class TakePictureActivity extends AppCompatActivity {
 
     private static final int REQUEST_IMAGE_CAPTURE = 1;
     private static final int REQUEST_EXTERNAL_STORAGE = 101;
     private static final int REQUEST_ALL = 255;
+    private File imgFile;
     private ImageView imageView;
 
     @Override
@@ -29,7 +39,7 @@ public class TakePictureActivity extends AppCompatActivity {
                     Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
                     || ContextCompat.checkSelfPermission(TakePictureActivity.this,
                     Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                //todo 在这里申请相机、存储的权限 !done
+                //申请相机、存储的权限
                 ActivityCompat.requestPermissions(this,
                         new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE},
                         REQUEST_ALL);
@@ -41,31 +51,58 @@ public class TakePictureActivity extends AppCompatActivity {
     }
 
     private void takePicture() {
-        Toast.makeText(this, "success", Toast.LENGTH_LONG).show();
-        //todo 打开相机
+        //打开相机
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        imgFile = Utils.getOutputMediaFile(Utils.MEDIA_TYPE_IMAGE);
+        if (imgFile != null) {
+            Uri fileUri = FileProvider.getUriForFile(this, "com.bytedance.camera.demo", imgFile);
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            refreshAlbum();
             setPic();
         }
     }
 
+    private void refreshAlbum(){
+        //广播刷新相册
+        Intent intentBc = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        intentBc.setData(Uri.fromFile(imgFile));
+        this.sendBroadcast(intentBc);
+    }
+
     private void setPic() {
-        //todo 展示图片
-        //todo 根据imageView裁剪
-        //todo 根据缩放比例读取文件，生成Bitmap
-        //todo 如果存在预览方向改变，进行图片旋转
-        //todo 如果存在预览方向改变，进行图片旋转
+        //展示图片
+        //根据imageView裁剪
+        //根据缩放比例读取文件，生成Bitmap
+        //如果存在预览方向改变，进行图片旋转
+        int targetW = imageView.getWidth();
+        int targetH = imageView.getHeight();
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(imgFile.getAbsolutePath(), bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+        int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bmp = BitmapFactory.decodeFile(imgFile.getAbsolutePath(), bmOptions);
+        imageView.setImageBitmap(Utils.rotateImage(bmp, imgFile.getAbsolutePath()));
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode) {
             case REQUEST_ALL: {
-                //todo 判断权限是否已经授予 !done
+                //判断权限是否已经授予
                 if (grantResults.length == 2) {
                     if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
                         Toast.makeText(this, "拍照权限获取失败", Toast.LENGTH_LONG).show();
